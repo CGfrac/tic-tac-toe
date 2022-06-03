821a955f052a70698a40103622e312136703f06e
CGfrac <CGfrac@protonmail.com>
2022-06-03 14:15:30 +0200

    Implement tile change on click with player switching

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const gameBoard = (() => {
     let _board = Array(9).fill('');
 
-    const getBoard = () => { return _board };
+    const getBoard = () => {
+        return _board;
+    };
+
+    const getCell = index => {
+        return _board[index]
+    };
+
+    const setCell = (index, token) => {
+        _board[index] = token;
+    };
+
     const clearBoard = () => _board = Array(9).fill('');
 
     return {
         getBoard,
+        getCell,
+        setCell,
         clearBoard
     };
 })();
@@ -28,8 +41,16 @@ const displayController = (() => {
         }
     }
 
+    const updateCell = (index, token) => {
+        const cell = document.querySelector(`[data-index="${index}"]`);
+        const cellContent = cell.children[0];
+
+        cellContent.textContent = token;
+    };
+
     return {
-        drawboard
+        drawboard,
+        updateCell
     };
 })();
 
@@ -45,6 +66,36 @@ const player = token => {
 
 const game = (() => {
     const _players = [];
+    let _currentPlayer = 0;
+
+    const _getCurrentPlayer = () => {
+        return _players[_currentPlayer];
+    };
+
+    const _changeCurrentPlayer = () => {
+        _currentPlayer = (_currentPlayer + 1) % 2;
+    };
+
+    const _handleClick = (event) => {
+        const boardIndex = parseInt(event.target.getAttribute('data-index'));
+        const cell = gameBoard.getCell(boardIndex);
+
+        if (cell === '') {
+            const player = _getCurrentPlayer();
+            const token = player.getToken();
+
+            gameBoard.setCell(boardIndex, token);
+            displayController.updateCell(boardIndex, token);
+
+            _changeCurrentPlayer();
+        }
+    };
+
+    const _setEventListeners = () => {
+        const cells = document.querySelectorAll('.cell');
+
+        cells.forEach(cell => cell.addEventListener('click', _handleClick));
+    };
 
     const newGame = () => {
         displayController.drawboard();
@@ -52,6 +103,8 @@ const game = (() => {
         const player1 = player('X');
         const player2 = player('0');
         _players.push(player1, player2);
+
+        _setEventListeners();
     };
 
     return {
