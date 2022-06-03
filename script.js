2a1c259aae80ebc0b270ffc029eb4c2f77254e51
CGfrac <CGfrac@protonmail.com>
2022-06-03 13:33:53 +0200

    Implement player, game factory functions and clearBoard

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const gameBoard = (() => {
-    const _board = Array(9).fill('');
+    let _board = Array(9).fill('');
 
     const getBoard = () => { return _board };
+    const clearBoard = () => _board = Array(9).fill('');
 
     return {
-        getBoard
+        getBoard,
+        clearBoard
     };
 })();
 
@@ -31,4 +33,28 @@ const displayController = (() => {
     };
 })();
 
-displayController.drawboard();
+const player = token => {
+    const _token = token;
+    let _score = 0;
+
+    const getToken = () => { return _token };
+    const getScore = () => { return _score };
+};
+
+const game = (() => {
+    const _players = [];
+
+    const newGame = () => {
+        displayController.drawboard();
+
+        const player1 = player('X');
+        const player2 = player('0');
+        _players.push(player1, player2);
+    };
+
+    return {
+        newGame
+    };
+})();
+
+game.newGame();
