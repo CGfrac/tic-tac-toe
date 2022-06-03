cd5bcf4e229c48946270b2b46e6c2f7adff0ae30
CGfrac <CGfrac@protonmail.com>
2022-06-03 11:47:50 +0200

    Display board on webpage

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ const gameBoard = (() => {
         getBoard
     };
 })();
+
+const displayController = (() => {
+    const drawboard = () => {
+        const boardArray = gameBoard.getBoard();
+        const boardContainer = document.querySelector('#board');
+
+        for (const item of boardArray) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+
+            const cellContent = document.createElement('span');
+            cellContent.textContent = item;
+
+            cell.appendChild(cellContent);
+            boardContainer.appendChild(cell);
+        }
+    }
+
+    return {
+        drawboard
+    };
+})();
+
+displayController.drawboard();
